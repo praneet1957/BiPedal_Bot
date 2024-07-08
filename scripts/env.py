4ee1265dcbbb5aaa4e85a99a740a98e6e3cc04d8
praneet1957 <praneetdnayak@gmail.com>
2024-07-08 23:16:24 +0530

    adding models

--- scripts/env.py
+++ scripts/env.py
@@ -1,8 +1,26 @@
+import mujoco as mj
+from mujoco.glfw import glfw
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+
+
+
 class env:
     def __init__(self):
         self.physicsEng = 0
+        self.state = 
+
 
     def initialize(self):
-        env.state 
+        # randomly initialize state
+        self.state 
+
+
+    def step(self,state,action):
+
         
     
+    def 
